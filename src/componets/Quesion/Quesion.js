71f2854951edcf11d01a1d6baba99e16c22f81a0
Md Naeem talukder <naeemtalukder169135@gmail.com>
2022-03-26 23:19:19 +0600

    Final Commit Done

--- src/componets/Quesion/Quesion.js
+++ src/componets/Quesion/Quesion.js
@@ -6,6 +6,9 @@ const Quesion = () => {
         <div className='text'>
             <h3>Props vs State work?</h3>
             <p>Simply put, State is the local state of the component which cannot be accessed and modified outside of the component. It's equivalent to local variables in a function. Props, on the other hand, make components reusable by giving components the ability to receive data from their parent component in the form of props.</p>
+            <h3>How to useState work?</h3>
+            <p>The useState() is a Hook that allows you to have state variables in functional components. React has two types of components, one is class components which are ES6 classes that extend from React and the other is functional components. Class components a Component and can have state and lifecycle methods: class Message extends React. The useState hook is a special function that takes the initial state as an argument and returns an array of two entries.
+            </p>
             <h3>How React work?</h3>
             <p>It is maintained by Facebook. React uses a declarative paradigm that makes it easier to reason about your application and aims to be both efficient and flexible. It designs simple views for each state in your application, and React will efficiently update and render just the right component when your data changes</p>
 
