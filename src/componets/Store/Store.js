0905831faf335c4a680302f6c3643e2a16e50b96
Md Naeem talukder <naeemtalukder169135@gmail.com>
2022-03-26 16:19:13 +0600

    condition check

--- src/componets/Store/Store.js
+++ src/componets/Store/Store.js
@@ -6,16 +6,27 @@ import Camera from '../Cameras/Camera/Camera';
 const Store = () => {
     const [cameras, setCameras] = useState([]);
     const [cart, setCart] = useState([]);
+    const [choose, setChoose] = useState([]);
 
     useEffect(() => {
         fetch('cameras.json')
             .then(res => res.json())
             .then(data => setCameras(data))
     }, []);
+    if (cart.length < 4) {
+        var handleAddToCart = (camera) => {
+            const newCart = [...cart, camera];
+            setCart(newCart);
+        }
+    }
 
-    const handleAddToCart = (camera) => {
-        const newCart = [...cart, camera];
-        setCart(newCart);
+    const againHandler = () => {
+        setCart([]);
+    }
+
+    const chooseForMe = () => {
+        const getRandom = cart[(Math.random() * cart.length) | 0];
+        setChoose(getRandom);
     }
 
     return (
@@ -30,13 +41,16 @@ const Store = () => {
                 }
             </div>
             <div className="card-container">
+                <h2>Selected Camera</h2>
                 {
                     cart.map((item) => (<h3 key={item.id}>{item.name}</h3>))
                 }
-                < button > Choose For Me</button><button>Choses Again</button>
+                <div className='cart-btn'>
+                    < button onClick={chooseForMe} className='btn-choose'> Choose 1 For Me</button>
+                    <button onClick={againHandler} className='btn-again'>Choose Again</button>
+                </div>
             </div>
         </div >
     );
 };
-
 export default Store;
